712f5689f0ae78320df991ef3eb85b87c1190bb2
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-03-07 17:05:19 -0500

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,41 @@
+public class Point {
+
+  /* Global Variables */
+  private int x;
+  private int y;
+
+  /* Constructor */
+  Point(int a, int b){
+    this.x = a;
+    this.y = b;
+  }
+
+  Point(){
+    this.x = 0;
+    this.y = 0;
+  }
+
+  /* Accessor Mehods */
+  public int getX(){
+    return this.x;
+  }
+
+  public int getY(){
+    return this.y;
+  }
+
+  /* Modifier Methods */
+  public void setX(int a){
+    this.x = a;
+  }
+
+  public void setY(int a){
+    this.y = a;
+  }
+
+  /* Calculator Methods */
+
+  public String toString() {
+    return "x:" + this.x + " y:" + this.y;
+  }
+  }
